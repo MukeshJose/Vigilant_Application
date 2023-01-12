610b498e117690f8246195826c23cd4bf98ca744
Administrator <mukeshreus21@gmail.com>
2023-01-12 22:30:54 +0530

    new activities added

--- app/src/main/java/com/example/vigilantapplication/clientside/ClientLogin.java
+++ app/src/main/java/com/example/vigilantapplication/clientside/ClientLogin.java
@@ -9,10 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.vigilantapplication.R;
+import com.example.vigilantapplication.officerside.OfficerLogin;
 
 public class ClientLogin extends AppCompatActivity {
 
-    Button goToButton;
+    Button goToButton, loginButton;
     TextView registerButton;
 
     @Override
@@ -20,12 +21,13 @@ public class ClientLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_login);
 
-        goToButton = findViewById(R.id.account_switch_button);
-        registerButton = findViewById(R.id.register_button);
+        goToButton = findViewById(R.id.bt_account_switch_button);
+        registerButton = findViewById(R.id.tv_register_button);
+        loginButton = findViewById(R.id.bt_login_button);
         goToButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ClientLogin.this, Complaint_Form.class);
+                Intent intent = new Intent(ClientLogin.this, OfficerLogin.class);
                 startActivity(intent);
             }
         });
@@ -34,6 +36,14 @@ public class ClientLogin extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ClientLogin.this, ClientRegistration.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ClientLogin.this, ClientComplainForm.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/vigilantapplication/clientside/ClientRegistration.java
+++ app/src/main/java/com/example/vigilantapplication/clientside/ClientRegistration.java
@@ -1,29 +1,42 @@
 package com.example.vigilantapplication.clientside;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vigilantapplication.R;
 
 public class ClientRegistration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
     String[] IdTypes = {"--Select an ID--", "Aadhar card", "Driving License", "Pan Card"};
+    private Button btRegisterButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_registration);
 
-        spinner = findViewById(R.id.id_type);
+        spinner = findViewById(R.id.sp_id_type);
         spinner.setOnItemSelectedListener(ClientRegistration.this);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, IdTypes);
         adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        initView();
+
+        btRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ClientRegistration.this, ViewClientProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -34,5 +47,9 @@ public class ClientRegistration extends AppCompatActivity implements AdapterView
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void initView() {
+        btRegisterButton = findViewById(R.id.bt_register_button);
     }
 }
