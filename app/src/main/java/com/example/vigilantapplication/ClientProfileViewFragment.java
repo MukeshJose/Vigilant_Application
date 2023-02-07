a866219737fc830a95c12272148660e4d5862b2f
Administrator <mukeshreus21@gmail.com>
2023-02-07 15:02:16 +0530

    Officer Login API integration and addition of saripaar library completed
    MUKESH JOSE  06/02/2023

--- app/src/main/java/com/example/vigilantapplication/ClientProfileViewFragment.java
+++ app/src/main/java/com/example/vigilantapplication/ClientProfileViewFragment.java
@@ -1,23 +1,82 @@
 package com.example.vigilantapplication;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+
+import com.example.vigilantapplication.model.Root;
+import com.example.vigilantapplication.retrofit.APIClient;
+import com.example.vigilantapplication.retrofit.APIInterface;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class ClientProfileViewFragment extends Fragment {
+
+
+    private CardView cvDisplayImage;
+    private TextView tvUserName;
+    private TextView tvIdType;
+    private TextView tvIdNumber;
+    private TextView tvPhoneNumber;
+    private Button btEditProfileButton;
+
+    public ClientProfileViewFragment() {
+    }
+
+    static String phoneNumber, passWord, deviceToken;
+
+    public ClientProfileViewFragment(String phone_number, String pass_word, String device_token) {
+        phoneNumber = phone_number;
+        passWord = pass_word;
+        deviceToken = device_token;
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.fragment_client_profile_view, container, false);
+        initView(view);
+
+        APIInterface api_profile_display = APIClient.getClient().create(APIInterface.class);
+        api_profile_display.CALL_APIUserLogin(phoneNumber, passWord, deviceToken).enqueue(new Callback<Root>() {
+            @Override
+            public void onResponse(Call<Root> call, Response<Root> response) {
+                if (response.isSuccessful()) {
+                    Root root = response.body();
+                    if (root.status) {
+                        tvUserName.setText(root.driverDetails.get(0).name);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Root> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
+    }
+
+    private void initView(View view) {
+        cvDisplayImage = view.findViewById(R.id.cv_display_image);
+        tvUserName = view.findViewById(R.id.tv_user_name);
+        tvIdType = view.findViewById(R.id.tv_id_type);
+        tvIdNumber = view.findViewById(R.id.tv_id_number);
+        tvPhoneNumber = view.findViewById(R.id.tv_phone_number);
+        btEditProfileButton = view.findViewById(R.id.bt_edit_profile_button);
     }
 }
--- app/src/main/java/com/example/vigilantapplication/clientside/ClientRegistration.java
+++ app/src/main/java/com/example/vigilantapplication/clientside/ClientRegistration.java
@@ -1,11 +1,14 @@
 package com.example.vigilantapplication.clientside;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +19,15 @@ import com.example.vigilantapplication.model.Root;
 import com.example.vigilantapplication.retrofit.APIClient;
 import com.example.vigilantapplication.retrofit.APIInterface;
 import com.google.android.material.textfield.TextInputEditText;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
 import com.mobsandgeeks.saripaar.annotation.Email;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
+
+
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
@@ -27,15 +35,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ClientRegistration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class ClientRegistration extends AppCompatActivity implements AdapterView.OnItemSelectedListener, Validator.ValidationListener, View.OnClickListener {
 
     String[] IdTypes = {"--Select an ID--", "Aadhar card", "Driving License", "Pan Card"};
     private Button btRegisterButton;
     private CardView cvDisplayImage;
     private CardView cvDpEditButton;
-    @NotEmpty
+    @NotEmpty(message = "enter the user name")
     private TextInputEditText etUserName;
-    @NotEmpty
+
     private Spinner spIdType;
     @NotEmpty
     private TextInputEditText etIdNumber;
@@ -47,12 +55,14 @@ public class ClientRegistration extends AppCompatActivity implements AdapterView
     @NotEmpty
     private TextInputEditText etPhoneNumber;
     @NotEmpty
-    @Password
+    @Password(min = 6, message = "password should have minimum 6 characters")
     private TextInputEditText etPassword;
     @NotEmpty
-    @ConfirmPassword
+    @ConfirmPassword(message = "passwords do not match")
     private TextInputEditText etConfirmPassword;
     String id_type_selection;
+    private Validator validator;
+    protected boolean validated;
 
 
     @Override
@@ -61,56 +71,32 @@ public class ClientRegistration extends AppCompatActivity implements AdapterView
         setContentView(R.layout.activity_client_registration);
 
         initView();
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+
         spIdType.setOnItemSelectedListener(ClientRegistration.this);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, IdTypes);
         adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
         spIdType.setAdapter(adapter);
 
 
-        btRegisterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent intent = new Intent(ClientRegistration.this, ViewClientProfile.class);
-//                startActivity(intent);
+//        btRegisterButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
 
-                RequestBody name = RequestBody.create(MediaType.parse("text/plain"), etUserName.getText().toString());
-                RequestBody phone = RequestBody.create(MediaType.parse("text/plain"), etPhoneNumber.getText().toString());
-                RequestBody email = RequestBody.create(MediaType.parse("text/plain"), etEmailId.getText().toString());
-                RequestBody password = RequestBody.create(MediaType.parse("text/plain"), etPassword.getText().toString());
-                RequestBody address = RequestBody.create(MediaType.parse("text/plain"), etAddress.getText().toString());
-                RequestBody id_type = RequestBody.create(MediaType.parse("text/plain"), id_type_selection);
-                RequestBody id_number = RequestBody.create(MediaType.parse("text/plain"), etIdNumber.getText().toString());
-
-                APIInterface api_registration = APIClient.getClient().create(APIInterface.class);
-
-                api_registration.CALL_APIRegistration(name, email, phone, password, address, id_type, id_number).enqueue(new Callback<Root>() {
-                    @Override
-                    public void onResponse(Call<Root> call, Response<Root> response) {
-                        if (response.isSuccessful()) {
-                            Root root = response.body();
-                            if (root.status) {
-                                Toast.makeText(ClientRegistration.this, "Registration is Successful", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(ClientRegistration.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                            }
-                        }else{
-                            Toast.makeText(ClientRegistration.this, "Reg error", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<Root> call, Throwable t) {
-                        Toast.makeText(ClientRegistration.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-            }
-        });
+//
+//            }
+//        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
         id_type_selection = IdTypes[i];
+
     }
 
     @Override
@@ -131,5 +117,89 @@ public class ClientRegistration extends AppCompatActivity implements AdapterView
         etPhoneNumber = findViewById(R.id.et_phone_number);
         etPassword = findViewById(R.id.et_password);
         etConfirmPassword = findViewById(R.id.et_confirm_password);
+
+        btRegisterButton.setOnClickListener(this);
+
+
     }
+
+    protected boolean validate() {
+        if (validator != null)
+            validator.validate();
+        return validated;           // would be set in one of the callbacks below
+    }
+
+    @Override
+    public void onClick(View view) {
+        validator.validate();
+        if (validated){
+
+            if (id_type_selection == "--Select an ID--") {
+                Toast.makeText(ClientRegistration.this, "Please select an ID type", Toast.LENGTH_SHORT).show();
+            }
+
+            RequestBody name = RequestBody.create(MediaType.parse("text/plain"), etUserName.getText().toString());
+            RequestBody phone = RequestBody.create(MediaType.parse("text/plain"), etPhoneNumber.getText().toString());
+            RequestBody email = RequestBody.create(MediaType.parse("text/plain"), etEmailId.getText().toString());
+            RequestBody password = RequestBody.create(MediaType.parse("text/plain"), etPassword.getText().toString());
+            RequestBody address = RequestBody.create(MediaType.parse("text/plain"), etAddress.getText().toString());
+            RequestBody id_type = RequestBody.create(MediaType.parse("text/plain"), id_type_selection);
+            RequestBody id_number = RequestBody.create(MediaType.parse("text/plain"), etIdNumber.getText().toString());
+
+            APIInterface api_registration = APIClient.getClient().create(APIInterface.class);
+
+            api_registration.CALL_APIRegistration(name, email, phone, password, address, id_type, id_number).enqueue(new Callback<Root>() {
+                @Override
+                public void onResponse(Call<Root> call, Response<Root> response) {
+                    if (response.isSuccessful()) {
+                        Root root = response.body();
+                        if (root.status) {
+                            Toast.makeText(ClientRegistration.this, "Registration is Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), ClientLogin.class));
+                        } else {
+                            Toast.makeText(ClientRegistration.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(ClientRegistration.this, "Reg error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Root> call, Throwable t) {
+                    Toast.makeText(ClientRegistration.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+        validated = true;
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        validated = false;
+
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+
+            // Display error messages
+            if (view instanceof Spinner) {
+                Spinner sp = (Spinner) view;
+                view = ((LinearLayout) sp.getSelectedView()).getChildAt(0);        // we are actually interested in the text view spinner has
+            }
+
+            if (view instanceof TextView) {
+                TextView et = (TextView) view;
+                et.setError(message);
+            }
+        }
+
+    }
+
 }
